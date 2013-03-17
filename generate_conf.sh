a3acc1e209edc6853e99b0d64999d11e40912beb
Ivan Malison <IvanMalison@gmail.com>
2013-03-17 02:25:34 -0700

    o Please enter the commit message for your changes. Lines starting

---
+++ generate_conf.sh
@@ -0,0 +1,25 @@
+cd "$(dirname "${BASH_SOURCE}")"
+source themes/default.sh &>/dev/null
+
+function writeTmuxConf() {
+    echo 'set-option -g status on
+set-option -g status-interval 2
+set-option -g status-utf8 on
+set-option -g status-justify "centre"
+set-option -g status-left-length 60
+set-option -g status-right-length 90
+set-option -g status-left "#('`pwd`'/powerline.sh left)"
+set-option -g status-right "#('`pwd`'/powerline.sh right)"
+set-option -g status-bg colour'$TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR'
+set-option -g status-fg colour'$TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR > ~/.tmux.conf
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    writeTmuxConf
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+	writeTmuxConf
+    fi
+fi
