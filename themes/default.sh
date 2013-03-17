9bca7f9c7dc533dc636448a0095d289479e96a87
Ivan Malison <IvanMalison@gmail.com>
2013-03-16 22:59:49 -0700

    Customized some stuff.

--- themes/default.sh
+++ themes/default.sh
@@ -23,17 +23,12 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
-		"hostname 33 0" \
+		#"tmux_session_info 148 234" \
+		"hostname 21 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
 		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		"wan_ip 24 255" \
-		"vcs_branch 29 88" \
-		"vcs_compare 60 255" \
-		"vcs_staged 64 255" \
-		"vcs_modified 9 255" \
-		"vcs_others 245 0" \
 	)
 fi
 
@@ -42,17 +37,22 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"earthquake 3 0" \
 		"pwd 89 211" \
 		"mailcount 9 255" \
-		"now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 237 167" \
+		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
-		"weather 37 255" \
+		#"battery 137 127" \
+		#"weather 37 255" \
 		#"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"vcs_branch 24 255" \
+		"vcs_compare 60 255" \
+		"vcs_staged 64 255" \
+		"vcs_modified 9 255" \
+		"vcs_others 245 0" \
+		"now_playing 234 37" \
+		#"date_day 235 136" \
+		"date 21 0" \
+		"time 21 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
