7a041fb7b7b612bbd9459648d89b17cdc411dd97
Ivan Malison <imalison@IvanMalison.(none)>
2013-03-18 09:36:27 -0700

    Updated configuration; Made it so that the prompt has new stuff now.

--- generate_conf.sh
+++ generate_conf.sh
@@ -12,6 +12,9 @@ set-option -g status-left "#('`pwd`'/powerline.sh left)"
 set-option -g status-right "#('`pwd`'/powerline.sh right)"
 set-option -g status-bg colour'$TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR'
 set-option -g status-fg colour'$TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR > ~/.tmux.conf
+echo '
+' >> ~/.zshrc
+echo 'PS1="$PS1"''$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")' >> ~/.zshrc
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
