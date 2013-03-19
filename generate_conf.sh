cf9f6236449135d5c803198c66aacb731d588eaf
Ivan Malison <imalison@yelp.com>
2013-03-19 14:17:12 -0700

    Made zsh the default shell for tmux.

--- generate_conf.sh
+++ generate_conf.sh
@@ -2,7 +2,8 @@ cd "$(dirname "${BASH_SOURCE}")"
 source themes/default.sh &>/dev/null
 
 function writeTmuxConf() {
-    echo 'set-option -g status on
+    echo 'set-option -g default-shell /bin/zsh
+set-option -g status on
 set-option -g status-interval 2
 set-option -g status-utf8 on
 set-option -g status-justify "centre"
