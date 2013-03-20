7b1bf62614b34b4108e6916522a342ec17fa2d27
Ivan Malison <imalison@yelp.com>
2013-03-20 10:37:57 -0700

    Revert "update way we get zsh path."
    This reverts commit 104d5b111d2d39e51382838410f7ece006064fa3.

--- generate_conf.sh
+++ generate_conf.sh
@@ -2,7 +2,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 source themes/default.sh &>/dev/null
 
 function writeTmuxConf() {
-    echo 'set-option -g default-shell `which zsh`
+    echo 'set-option -g default-shell /bin/zsh
 set-option -g status on
 set-option -g status-interval 2
 set-option -g status-utf8 on
